1c2c59424f61509879d97d38dfacdc8d22dc06fa
ioanteacoe-droid <ioan.teacoe@ulbsibiu.ro>
2026-03-11 17:51:11 +0200

    Laborator_1

---
+++ Laborator_1.py
@@ -0,0 +1,31 @@
+type(1)
+print(type(10))
+print(10-3)
+pow(5,2)
+print(pow(5,2))
+abs(-50)
+print(abs(-50))
+print(round(5,46))
+print(hex(512))
+
+
+#age = input("How are you?")
+#pi = input("3.14")
+#pi = float(pi)
+
+# strings
+name = "Ioan"
+print(name[2])
+print(name[:])
+print(name[3])
+
+print("*"*29)
+
+  # boolean
+print(bool(None))
+print(bool(False))
+print(bool(1))
+
+
+
+
