e15987f063a0b629477f4e462b058c087813f529
ioanteacoe-droid <ioan.teacoe@ulbsibiu.ro>
2026-04-29 17:36:58 +0300

    NUMPY
    Laborator ML

---
+++ laborator8.py
@@ -0,0 +1,12 @@
+import numpy as np
+a = np.array([1,2,3])
+print(a)
+print(type(a))
+print(a.dtype)
+print(a.shape)
+print(a[0])
+
+ b = np.array([[1,2,3], [4,5,6]])
+ print(b.shape)
+ print(b[0][2])
+ print(b[0,2])
